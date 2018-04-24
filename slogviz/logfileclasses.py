d9d2654ec7185bcc6a1e1fa4fd8b3e8dd371cb2e
Marius Frinken <m-frinken@gmx.de>
2018-04-24 15:29:55 +0200

    added documenatation and renamed functions

--- slogviz/logfileclasses.py
+++ slogviz/logfileclasses.py
@@ -51,14 +51,24 @@ def _select_entries_param(list, param):
 
 #START exported classes
 class logfile_entry(object):
-	"""A class for storing one single log file entry
+	"""A class for storing one single log file entry.
+
+	Attributes:
+	id -- a numerical id, assumed to be assigned sequentially in time
+	      the id shall be unique among all entries from one file
+	origin_name -- a string containing the name of the origin log file
+	message -- the message of an entry, a string
+	structured_data -- the whole data of an entry, kept in the original structure
+	timestamp -- a datetime object assumed to be in UTC format
+	hostname -- a string representing the hostname that logged the entry
+	source --  string representing the application/other source that logged the entry
 	"""
 	def __init__(self, id, origin_name, message, structured_data, timestamp, hostname, source):
 		self.id = id
 		self.origin_name = origin_name
 		self.message = message
 		self.structured_data = structured_data
-		self.timestamp = timestamp # a datetime object assumed to be in UTC format
+		self.timestamp = timestamp
 		self.hostname = hostname
 		self.source = source
 
@@ -66,7 +76,19 @@ class logfile_entry(object):
 		return 'Entry {0.id} at {0.timestamp} from file {0.origin_name}, hostname {0.hostname} and source {0.source} with the message:\n {0.message}'.format(self)
 
 class logfile(object):
-	"""A class for storing content and meta data of one log file
+	"""A class for storing content and meta data of one log file.
+
+	Attributes:
+	name -- a string, the name of the original file
+	lines -- a number counting the amount of logfile_entry object inside the content attribute
+	type -- the file format of the original file
+	content --  a list of logfile_entry objects that represent the content of the original file
+	sources --  a list of strings representing the various source attribute occurring in all elements of the content list
+
+	Exported Functions:
+	give_plot_data -- returns the necessary data for plotting, called by the plotter sub module
+	give_plot_data_bar --  returns the necessary data for plotting a bar chart, called by the plotter sub module
+	export_to_JSON -- saves the object as a JSON file for future analysis
 	"""
 	def __init__(self, name, lines, type, content, sources):
 		self.name = name
@@ -78,24 +100,7 @@ class logfile(object):
 	def __str__(self):
 		return 'File of the type {0.type} with the name {0.name} and {0.lines} lines'.format(self)
 
-	def give_plot_data(self, remove_redundant_entries=0, sources=[]):
-		"""Filters the entries in the self.content attribute and returns the filtered list, their respective ids, timestamps and messages.
-		All list returned are sorted by the id of their respective log file entry.
-
-		Keyword arguments:
-		remove_redundant_entries -- If set to 1, all entries with the same timestamp will be condensed to one (default 0)
-		sources -- A list of sources, entries not in this list will be filtered out. If this list is empty, no filtering will occur. (default [])
-		"""
-		ret = []
-		if remove_redundant_entries:
-			ret = self.remove_redundant_entries()
-		else:
-			ret = self.content
-		ret = _select_entries_param(ret,sources)
-		ret.sort(key=lambda x: x.id)
-		return ret, _give_ids(ret, remove_redundant_entries), _give_dates(ret), _give_messages(ret)
-
-	def remove_redundant_entries(self):
+	def _remove_redundant_entries(self):
 		"""Returns the self.content list, where all entries will have different timestamps.
 		This is achieved by keeping the first occurrence of each timestamp.
 		"""
@@ -107,6 +112,23 @@ class logfile(object):
 				del dict[counter]
 			oldvalue=v.timestamp
 		return [v for k,v in dict.items()]
+
+	def give_plot_data(self, remove_redundant_entries=0, sources=[]):
+		"""Filters the entries in the self.content attribute and returns the filtered list, their respective ids, timestamps and messages.
+		All list returned are sorted by the id of their respective log file entry.
+
+		Keyword arguments:
+		remove_redundant_entries -- If set to 1, all entries with the same timestamp will be condensed to one (default 0)
+		sources -- A list of sources, entries not in this list will be filtered out. If this list is empty, no filtering will occur. (default [])
+		"""
+		ret = []
+		if remove_redundant_entries:
+			ret = self._remove_redundant_entries()
+		else:
+			ret = self.content
+		ret = _select_entries_param(ret,sources)
+		ret.sort(key=lambda x: x.id)
+		return ret, _give_ids(ret, remove_redundant_entries), _give_dates(ret), _give_messages(ret)
 
 	def give_plot_data_bar(self, frame_seconds=0):
 		"""Counts how many entries in self.content in frames of the size frame_seconds exist and returns three lists.
--- slogviz/plotter.py
+++ slogviz/plotter.py
@@ -4,6 +4,14 @@
 
 Exported functions:
 
+show -- makes plots visible, has to be called when plotting
+plot_single -- plots the timestamps of one logfile to the X-axis and the ids to the Y-axis
+plot_single_file_colored -- produces the same as plot_single, but data points are colored by their source attribute
+plot_timeline_overview -- plots the timestamps of a set of logfiles to the X-axis, where all loaded log files are split to different heights on the Y-axis
+plot_multiple_timeline -- produces a plot with the same axis as the plot_single function, but here all loaded log files are displayed in one set of coordinates
+plot_bar_chart -- produces a plot where the timestamps of logfile_entries are displayed on the X-axis, while the Y-axis is used to indicate the amount of entries
+superplot -- produces all plots at once
+plot_correlated -- takes a set of logfile_entries and plots them very similarly to the plot_timeline_overview
 """
 
 import matplotlib.pyplot as plt
@@ -12,10 +20,20 @@ from datetime import timedelta
 from matplotlib.dates import DateFormatter
 
 #from .logfileclasses import *
-# this import is not necessary because of the overall structure, but keep in mind that classes and functions defined there are used here
+# this import is not necessary because of the overall structure,
+# but keep in mind that classes defined there are used here
 
 #START internal functions
 def _transform_select_string(select_string, logfile):
+	"""This function takes a string and a logfile and returns a list of strings.
+
+	Positional Arguments:
+	selected_string -- a string that should contain sources, trailed by a ','
+	logfile -- a logfile object
+
+	Returns:
+	a list of strings, representing all sources that are found in the selected_string and in the logfile
+	"""
 	selected_sources = []
 	tmp = select_string.split(',')
 	for s in tmp:
@@ -26,20 +44,29 @@ def _transform_select_string(select_string, logfile):
 	return selected_sources
 
 def _quit_figure(event):
-	"""Helper function for closing pyplot windows with the 'q' key"""
+	"""Helper function for closing pyplot windows with the 'q' key."""
 	if event.key == 'q':
 		plt.close(event.canvas.figure)
 #END internal functions
 
+#START exported functions
 def show():
 	"""Finally produces one ore multiple pyplot plots.
 	Has to be called once after one calling any of the other public functions in order to see a plot.
 	"""
 	plt.show()
 
-#START plots
 def plot_single(log, remove_redundant_entries, select_string):
-	"""Plots the Data from one logfile onto one system of coordinates"""
+	"""Plots the Data from one logfile object onto one system of coordinates.
+	It plots the timestamps of one logfile to the X-axis and the ids to the Y-axis.
+
+	Positional Arguments:
+	log -- the logfile object to plot
+	remove_redundant_entries -- a binary digit, when set to 1,
+		all entries from the same logfile with the same timestamp will be removed, except for one
+	select_string -- a string that should contain sources, trailed by a ',',
+		used for filtering out all entries that have sources which are NOT in the select_string
+	"""
 	selected_sources = _transform_select_string(select_string,log)
 	plot_data , lines, dates, _ = log.give_plot_data(remove_redundant_entries=remove_redundant_entries, sources=selected_sources)
 	fig, ax = plt.subplots(figsize=(11,7))
@@ -47,13 +74,19 @@ def plot_single(log, remove_redundant_entries, select_string):
 	l, = plt.plot(dates,lines,picker=10, color='red', marker='.', linestyle='-', linewidth=0.5, ms=5, mec='blue', label=log.name )
 	myFmt = DateFormatter("%Y %d.%b %H:%M:%S")
 	ax.xaxis.set_major_formatter(myFmt)
-	plt.title('Analysis of the file \"'+log.name+'\"')
 	plt.legend(loc='upper left')
-	plt.subplots_adjust(left=0.1, bottom=0.23, right=0.9, top=0.95)
 	annot = ax.annotate("", xy=(0,0), xytext=(0.01,0.01) ,textcoords='figure fraction', bbox=dict(boxstyle="round", fc="cyan"), arrowprops=dict(arrowstyle="->"))
 	annot.set_visible(False)
-	ax.set_xlabel('timestamp')
-	ax.set_ylabel('sequential id')
+	ax.set_xlabel('timestamps in UTC')
+
+	if remove_redundant_entries == 1:
+		ax.set_ylabel('number of sequential entry')
+		plt.title('Analysis of the files ' + log.name +'\n' + 'where all entries having the same timestamp are removed')
+		plt.subplots_adjust(left=0.1, bottom=0.23, right=0.95, top=0.90)
+	else:
+		plt.title('Analysis of the files ' + log.name)
+		ax.set_ylabel('sequential id')
+		plt.subplots_adjust(left=0.1, bottom=0.23, right=0.95, top=0.95)
 
 	def update_annot(ind):
 		x,y = l.get_data()
@@ -82,42 +115,67 @@ def plot_single(log, remove_redundant_entries, select_string):
 	fig.canvas.mpl_connect("motion_notify_event", hover)
 	fig.canvas.mpl_connect('key_press_event', _quit_figure)
 
-def plot_single_file_multiple_sources(log, remove_redundant_entries, select_string):
-	"""
+def plot_single_file_colored(log, remove_redundant_entries, select_string, rev=True):
+	"""Plots the Data from one logfile object onto one system of coordinates.
+	It plots the timestamps of one logfile to the X-axis and the ids to the Y-axis.
+	Additionally, each data point is colored according to the source of the related logfile_entry.
+
+	Positional Arguments:
+	log -- the logfile object to plot
+	remove_redundant_entries -- a binary digit, when set to 1,
+		all entries from the same logfile with the same timestamp will be removed, except for one
+	select_string -- a string that should contain sources, trailed by a ',',
+		used for filtering out all entries that have sources which are NOT in the select_string
+
+	Keyword Arguments:
+	rev -- determines the order, in which the data points are drawn, True means that the most frequent sources are
+		drawn in the foreground (default True)
 	"""
 	selected_sources = _transform_select_string(select_string,log)
 
 	fig, ax = plt.subplots(figsize=(11,7))
 	fig.autofmt_xdate()
 	plot_data, _ , _ , _ = log.give_plot_data(remove_redundant_entries=remove_redundant_entries)
-	color_map = plt.get_cmap('gnuplot2')
+	color_map = plt.get_cmap('gist_ncar')
+	# see https://matplotlib.org/users/colormaps.html
 	color_index = [color_map(1.*i/len(selected_sources)) for i in range(0,len(selected_sources))]
 	ax.set_prop_cycle(color=color_index)
 
+	all_dates = []
+	all_lines = []
+	all_names = []
+
 	ls = []
-	counter = 0
 	for s in selected_sources:
-		dates = [x.timestamp for x in plot_data if x.source == s]
+		all_dates.append([x.timestamp for x in plot_data if x.source == s])
 		if remove_redundant_entries == 1:
-			lines = [plot_data.index(x) for x in plot_data if x.source == s]
+			all_lines.append([plot_data.index(x) for x in plot_data if x.source == s])
 		else:
-			lines = [x.id for x in plot_data if x.source == s]
+			all_lines.append([x.id for x in plot_data if x.source == s])
 		breakline_s = s
 		if len(breakline_s) >= 23:
 			for i in range(0,int(len(breakline_s)/23)):
 				breakline_s = breakline_s[:22] + '\n' + breakline_s[22:]
-		tmp, = ax.plot(dates,lines, label=breakline_s,marker='.', linestyle='None', linewidth=0.05, ms=10, picker=10)
+		all_names.append(breakline_s)
+
+	list_ind = list(range(0,len(all_names)))
+	list_ind.sort(key=lambda x: len(all_dates[x]), reverse=rev)
+
+	for i in list_ind:
+		tmp, = ax.plot(all_dates[i], all_lines[i], label=all_names[i], marker='.', linestyle='None', linewidth=0.05, ms=7, picker=10)
 		ls.append(tmp)
-		counter += 1
+
+
 	myFmt = DateFormatter("%d.%b %H:%M")
 
 	ax.xaxis.set_major_formatter(myFmt)
+
 
 	plt.legend(loc='upper left', bbox_to_anchor=(0, 0.95), bbox_transform=plt.gcf().transFigure)
 
 	annot = ax.annotate("", xy=(0,0), xytext=(0.23,0.01) ,textcoords='figure fraction', bbox=dict(boxstyle="round", fc="cyan"), arrowprops=dict(arrowstyle="->"))
 	annot.set_visible(False)
-	ax.set_xlabel('timestamp')
+	ax.set_xlabel('timestamps in UTC')
 	if remove_redundant_entries == 1:
 		ax.set_ylabel('number of sequential entry')
 		plt.title('Analysis of the files ' + log.name +'\n' + 'where all entries having the same timestamp are removed')
@@ -155,8 +213,13 @@ def plot_single_file_multiple_sources(log, remove_redundant_entries, select_stri
 	fig.canvas.mpl_connect("motion_notify_event", hover)
 	cid = plt.gcf().canvas.mpl_connect('key_press_event', _quit_figure)
 
-def plot_multiple_timeline(logs):
-	"""Plots the Data from logs onto one system of coordiantes"""
+def plot_timeline_overview(logs):
+	"""This function plots the timestamps of a set of logfiles to the X-axis,
+	while all loaded log files are split to different heights on the Y-axis.
+
+	Positional Arguments:
+	logs -- the logfile object to plot
+	"""
 	fig, ax = plt.subplots(figsize=(11,7))
 	fig.autofmt_xdate()
 	c = 0
@@ -164,7 +227,7 @@ def plot_multiple_timeline(logs):
 	plot_data_dict = {}
 	for l in logs:
 		plot_data, _, dates, _ = l.give_plot_data()
-		tmp, = ax.plot(dates, [c]*len(dates), label=l.name, picker=4, marker='.', linestyle='-', linewidth=0.05, ms=5)
+		tmp, = ax.plot(dates, [c]*len(dates), label=l.name, picker=10, marker='.', linestyle='-', linewidth=0.05, ms=5)
 		plot_data_dict[tmp.get_c()] = plot_data
 		line2D_array.append(tmp)
 		c += 1
@@ -180,7 +243,7 @@ def plot_multiple_timeline(logs):
 
 	annot = ax.annotate("", xy=(0,0), xytext=(0.01,0.01) ,textcoords='figure fraction', bbox=dict(boxstyle="round", fc="cyan"), arrowprops=dict(arrowstyle="->"))
 	annot.set_visible(False)
-	ax.set_xlabel('timestamps')
+	ax.set_xlabel('timestamps in UTC')
 	ax.set_ylabel('log files')
 
 	def update_annot(l,ind):
@@ -208,9 +271,17 @@ def plot_multiple_timeline(logs):
 	fig.canvas.mpl_connect("motion_notify_event", hover)
 	cid = plt.gcf().canvas.mpl_connect('key_press_event', _quit_figure)
 
-def plot_multiple(logs, remove_redundant_entries, select_string):
-	"""Plots the Data from logs onto one system of coordiantes"""
+def plot_multiple_timeline(logs, remove_redundant_entries, select_string):
+	"""Produces a plot with the same axis as the plot_single function,
+	but here all loaded log files are displayed in one set of coordinates.
 
+	Positional Arguments:
+	logs -- the logfile objects to plot
+	remove_redundant_entries -- a binary digit, when set to 1,
+		all entries from the same logfile with the same timestamp will be removed, except for one
+	select_string -- a string that should contain sources, trailed by a ',',
+		used for filtering out all entries that have sources which are NOT in the select_string
+	"""
 	fig, ax = plt.subplots(figsize=(11,7))
 	fig.autofmt_xdate()
 	line2D_array = []
@@ -235,7 +306,7 @@ def plot_multiple(logs, remove_redundant_entries, select_string):
 
 	annot = ax.annotate("", xy=(0,0), xytext=(0.01,0.01) ,textcoords='figure fraction', bbox=dict(boxstyle="round", fc="cyan"), arrowprops=dict(arrowstyle="->"))
 	annot.set_visible(False)
-	ax.set_xlabel('timestamp')
+	ax.set_xlabel('timestamps in UTC')
 	ax.set_ylabel('sequential id')
 
 	def update_annot(l,ind):
@@ -269,7 +340,16 @@ def plot_multiple(logs, remove_redundant_entries, select_string):
 
 	cid = plt.gcf().canvas.mpl_connect('key_press_event', _quit_figure)
 
-def scatter_plot(log, frame_seconds=-1):
+def plot_bar_chart(log, frame_seconds=-1):
+	"""This function produces a plot where the timestamps of logfile_entries are displayed on the X-axis, while the Y-axis is used to indicate the amount of entries.
+	The timestamps are categorized in frames of a certain size and the data is then visualized in the form of bars.
+
+	Positional Arguments:
+	log -- the logfile object to plot
+
+	Keyword Arguments:
+	frame_seconds -- the size of the time frames (default -1), if not given by the user, it will be calculated as seen below
+	"""
 	if frame_seconds == -1:
 		frame_seconds = (log.content[-1].timestamp - log.content[0].timestamp).total_seconds() / timedelta(seconds=100).total_seconds()
 	lines,dates,areas = log.give_plot_data_bar(frame_seconds=frame_seconds)
@@ -277,7 +357,7 @@ def scatter_plot(log, frame_seconds=-1):
 	fig.autofmt_xdate()
 	myFmt = DateFormatter("%Y %d.%b %H:%M:%S")
 	ax.xaxis.set_major_formatter(myFmt)
-	ax.set_xlabel('timestamp')
+	ax.set_xlabel('timestamps in UTC')
 	ax.set_ylabel('amount of entries')
 	plt.title('Analysis of the file \"'+log.name+'\" \n with frame size ' + str(frame_seconds) +' seconds')
 
@@ -288,18 +368,33 @@ def scatter_plot(log, frame_seconds=-1):
 	plt.subplots_adjust(left=0.15, bottom=0.2, right=0.9, top=0.9)
 	cid = fig.canvas.mpl_connect('key_press_event', _quit_figure)
 
+def superplot(logs, remove_redundant_entries, select_string):
+	"""Produces all plots at once.
 
-def superplot(logs,remove_redundant_entries,select_string):
+	Positional Arguments:
+	logs -- the logfiles object to plot
+	remove_redundant_entries -- a binary digit, when set to 1,
+		all entries from the same logfile with the same timestamp will be removed, except for one
+	select_string -- a string that should contain sources, trailed by a ',',
+		used for filtering out all entries that have sources which are NOT in the select_string
+	"""
 	for l in logs:
-		scatter_plot(l)
+		plot_bar_chart(l)
 		plot_single(l,remove_redundant_entries,select_string)
-		plot_single_file_multiple_sources(l, remove_redundant_entries, select_string)
-	plot_multiple(logs,remove_redundant_entries,select_string)
-	plot_multiple_timeline(logs)
+		plot_single_file_colored(l, remove_redundant_entries, select_string)
+	plot_multiple_timeline(logs,remove_redundant_entries,select_string)
+	plot_timeline_overview(logs)
 	show()
 
-
 def plot_correlated(list_of_entries, logfiles, rule_name):
+	"""Plots data that comes from an correlation rule.
+	Very similar to plot_timeline_overview.
+
+	Positional Arguments:
+	list_of_entries -- the list of logfile_entries to plot
+	logfiles -- the logfile objects from which elements from list_of_entries originate
+	rule_name -- the name of the correlation rule that returned the list_of_entries
+	"""
 	plot_data = list_of_entries
 	plot_data.sort(key=lambda x: x.id)
 
@@ -330,7 +425,7 @@ def plot_correlated(list_of_entries, logfiles, rule_name):
 	plt.subplots_adjust(left=0.23, bottom=0.2, right=0.9, top=t)
 	annot = ax.annotate("", xy=(0,0), xytext=(0.01,0.01) ,textcoords='figure fraction', bbox=dict(boxstyle="round", fc="cyan"), arrowprops=dict(arrowstyle="->"))
 	annot.set_visible(False)
-	ax.set_xlabel('timestamp')
+	ax.set_xlabel('timestamps in UTC')
 	ax.set_yticks(range(0,len(logfiles)))
 	ax.set_yticklabels([x.name for x in logfiles])
 
@@ -360,6 +455,4 @@ def plot_correlated(list_of_entries, logfiles, rule_name):
 	fig.canvas.mpl_connect("motion_notify_event", hover)
 	fig.canvas.mpl_connect('key_press_event', _quit_figure)
 
-
-
-#END plots
+#END exported functions
